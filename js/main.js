486bb509d7785b82064a6f2de3837c05b7b89001
alex-v-10 <alex.ru9311@gmail.com>
2022-10-04 22:30:06 +0300

    добавляет функцию, возвращающую случайное число с плавающей точкой из переданного диапазона включительно.

--- js/main.js
+++ js/main.js
@@ -10,7 +10,25 @@ function getRandomPositiveInt(min, max) {
     min = max;
     max = temp;
   }
+  if ((!Number.isInteger(min) && !Number.isInteger(max)) && (max - min < 1)) {
+      return NaN;
+  }
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getRandomPositiveFloat(min, max, digits) {
+  if (typeof min !== 'number' || typeof max !== 'number' || typeof digits !== 'number') {
+    return NaN;
+  }
+  if (min < 0 || max < 0 || digits < 0) {
+    return NaN;
+  }
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  return (Math.random() * (max - min) + min).toFixed(digits);
 }
