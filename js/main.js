40854b5823c67e3e4d433fb272aa301108540e51
alex-v-10 <alex.ru9311@gmail.com>
2022-10-04 15:17:07 +0300

    добавляет функцию, возвращающую случайное целое число из переданного диапазона включительно.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function getRandomPositiveInt(min, max) {
+  if (typeof min !== 'number' || typeof max !== 'number') {
+    return NaN;
+  }
+  if (min < 0 || max < 0) {
+    return NaN;
+  }
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
