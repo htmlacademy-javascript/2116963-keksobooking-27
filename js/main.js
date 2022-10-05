8a181b83410e3257efa804e103a77a492a0b5d7c
alex-v-10 <alex.ru9311@gmail.com>
2022-10-05 17:03:56 +0300

    исправляет ошибки

--- js/main.js
+++ js/main.js
@@ -1,37 +1,24 @@
-function getRandomPositiveInt(min, max) {
-  if (typeof min !== 'number' || typeof max !== 'number') {
-    return NaN;
-  }
+const getRandomPositiveInt = (min, max) => {
   if (min < 0 || max < 0) {
     return NaN;
   }
   if (min > max) {
-    const temp = min;
-    min = max;
-    max = temp;
-  }
-  if ((!Number.isInteger(min) && !Number.isInteger(max)) && (max - min < 1)) {
-    return NaN;
+    [min, max] = [max, min];
   }
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
-}
+};
 
-function getRandomPositiveFloat(min, max, digits) {
-  if (typeof min !== 'number' || typeof max !== 'number' || typeof digits !== 'number') {
-    return NaN;
-  }
+const getRandomPositiveFloat = (min, max, digits) => {
   if (min < 0 || max < 0 || digits < 0) {
     return NaN;
   }
   if (min > max) {
-    const temp = min;
-    min = max;
-    max = temp;
+    [min, max] = [max, min];
   }
-  return (Math.random() * (max - min) + min).toFixed(digits);
-}
+  return +((Math.random() * (max - min) + min).toFixed(digits));
+};
 
 getRandomPositiveInt(0, 0);
 getRandomPositiveFloat(0, 0, 0);
