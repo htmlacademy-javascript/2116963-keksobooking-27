853a5a5b64fee8728bdf3be7ba88dc79d0d1b25a
alex-v-10 <alex.ru9311@gmail.com>
2022-10-30 11:07:47 +0300

    исправляет замечания по 7 заданию

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 import { getOffers } from './data.js';
 import { createPopup } from './popup.js';
 import { addPopup } from './map.js';
-import { turnActive } from './form.js';
+import { turnFormOn } from './form.js';
+import { turnFiltersOn } from './map-filters.js';
 
 const popups = Array.from(getOffers(), (item) => createPopup(item));
 
 addPopup(popups[0]);
 
-turnActive();
+turnFormOn();
+turnFiltersOn();
