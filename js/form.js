32b0c5f6f508c8d62e93e5e2c313030a5c216d4a
alex-v-10 <alex.ru9311@gmail.com>
2022-11-01 02:03:44 +0300

    добавляет валидацию формы: заголовка, цены и количества мест

--- js/form.js
+++ js/form.js
@@ -1,4 +1,14 @@
 const adForm = document.querySelector('.ad-form');
+const addressField = adForm.querySelector('#address');
+const roomNumberField = adForm.querySelector('#room_number');
+const capacityField = adForm.querySelector('#capacity');
+
+const roomsToCapacity = {
+  1: ['1'],
+  2: ['1', '2'],
+  3: ['1', '2', '3'],
+  100: ['0'],
+};
 
 const turnFormOff = () => {
   adForm.classList.add('ad-form--disabled');
@@ -13,5 +23,48 @@ const turnFormOn = () => {
     element.disabled = false;
   });
 };
+
+const pristine = new Pristine(adForm, {
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form__element--invalid',
+  errorTextParent: 'ad-form__element',
+});
+
+const validateCapacity = (value) => roomsToCapacity[roomNumberField.value].includes(value);
+
+const capacityErrorMessage = () => {
+  if (roomNumberField.value === '100') {
+    return adForm.querySelector('[name="capacity"]').querySelector('[value="0"]').textContent;
+  }
+  return `Количество гостей: ${roomsToCapacity[roomNumberField.value].join(', ')}`;
+};
+
+pristine.addValidator(capacityField, validateCapacity, capacityErrorMessage);
+
+const onAddressFocus = () => {
+  addressField.blur();
+};
+
+const onRoomNumberChange = () => {
+  pristine.validate(capacityField);
+};
+
+const onCapacityChange = () => {
+  pristine.validate(capacityField);
+};
+
+addressField.addEventListener('focus', onAddressFocus);
+roomNumberField.addEventListener('change', onRoomNumberChange);
+capacityField.addEventListener('change', onCapacityChange);
+
+adForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  const isValid = pristine.validate();
+  if (isValid) {
+    console.log('form is valid');
+  } else {
+    console.log(pristine.getErrors());
+  }
+});
 
 export { turnFormOn, turnFormOff };
